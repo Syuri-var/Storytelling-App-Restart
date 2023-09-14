e5f39341cdbadc8316de998988194c48c3648824
Syuri-var <sayuri.yamamoto.555@gmail.com>
2023-09-14 14:07:56 +0900

    TOPページの音楽が次のページに行っても停止できません。

--- Storytelling App Restart/VideoTableViewController.swift
+++ Storytelling App Restart/VideoTableViewController.swift
@@ -1,98 +1,56 @@
-//
-//  TableViewController.swift
-//  Storytelling App Restart
-//
-//  Created by Apple on 2023/09/12.
-//
-
 import UIKit
 import AVFoundation
 
 class VideoTableViewController: UIViewController {
 
     var player: AVPlayer!
+    var audioPlayer: AVAudioPlayer?
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         // 動画ファイル名（Assets内のファイル名）を指定
-        let videoFileName = "銀河鉄道の夜 (1920 × 1080 px)" // 例: "sample_video.mp4"
-
+        let videoFileName = "銀河鉄道の夜 タイトル読み上げ" // 例: "sample_video.mp4"
+        
         // 動画ファイルのURLを取得
         if let videoURL = Bundle.main.url(forResource: videoFileName, withExtension: "mp4") {
             // AVPlayerを初期化し、AVPlayerLayerを作成
             player = AVPlayer(url: videoURL)
             let playerLayer = AVPlayerLayer(player: player)
-
+            
             // AVPlayerLayerのフレームを設定
             playerLayer.frame = view.bounds
-
+            
             // UIViewにAVPlayerLayerを追加
             view.layer.addSublayer(playerLayer)
-
+            
             // 動画再生を開始
             player.play()
         } else {
             print("動画ファイルが見つかりません")
-            
+        }
+        
+        // 音楽ファイル名（mp3ファイルなど）を指定
+        let musicFileName = "銀河鉄道の夜-優雅" // 例: "music.mp3"
+        
+        // 音楽ファイルのURLを取得
+        if let musicURL = Bundle.main.url(forResource: musicFileName, withExtension: "mp3") {
+            do {
+                // AVAudioPlayerのインスタンスを作成し、音楽を再生
+                audioPlayer = try AVAudioPlayer(contentsOf: musicURL)
+                audioPlayer?.play()
+            } catch {
+                print("音楽ファイルの再生エラー: \(error.localizedDescription)")
+            }
+        } else {
+            print("音楽ファイルが見つかりません")
+        }
+    }
 
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "NextScreenSegue" {
+            // 次の画面に遷移する前に音楽再生を停止
+            audioPlayer?.stop()
         }
     }
 }
-
-    /*
-    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
-
-        // Configure the cell...
-
-        return cell
-    }
-    */
-
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
-    }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
-    }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
-
